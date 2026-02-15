fb2ae8c073bea71ad3ef9ecff5d3103541d881cc
Vatsal <vatsalkhapre@iisc.ac.in>
2026-02-15 13:52:21 +0530

    deterministic conv

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
-python3 run_alphapre_convlstm_sevir_lr_latent.py \
-    --backbone alpha_afnoamplinet_latent_falfcl \
+CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm_sevir_lr_latent.py \
     --dataset meteo_lr_latent_32 \
     --exp_dir meteo_lr_latent_32 \
-    --exp_note "Testing_Integrity_with_afno_amplinet_0.01_1.0" \
-    --epochs 40 \
+    --exp_note "Testing_Integrity_with_afno_amplinet_0.01_1.0_deterministic" \
+    --epochs 25 \
     --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
     --valid \
     --num_workers 8 \
     --wandb_state 'online' \
     --wandb_project_name 'Alphapre' \
-    --run_name 'Afno_Amplinet_falfcl_only_meteonet_latent_32_.66'
+    --run_name 'Afno_Amplinet_falfcl_only_meteonet_latent_32_.21_deteministic'
--- run_alphapre_convlstm_sevir_lr_latent.py
+++ run_alphapre_convlstm_sevir_lr_latent.py
@@ -30,6 +30,7 @@ from datasets.get_datasets import get_dataset
 from utils.tools import print_log, cycle, show_img_info
 from copy import deepcopy
 from models.autoencoder_kl import AutoencoderKL
+torch.backends.cudnn.deterministic = True
 
 # Apply your own wandb api key to log online
 os.environ["WANDB_API_KEY"] = "6427ba1f8d0c13065720163c3aed0fa974031bef"

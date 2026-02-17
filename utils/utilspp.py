e43811aa312399ca7677799ce8e8fd45914c47fc
Vatsal Khapre <vatsalkhapre@iisc.ac.in>
2026-02-17 14:33:42 +0530

    some new experiments

--- utils/utilspp.py
+++ utils/utilspp.py
@@ -565,6 +565,60 @@ class RandomScheduling(nn.Module):
         # self.step += 1
         return loss
 
+
+class RandomScheduling_MSE(nn.Module):
+    def __init__(self, total_step, micro_batch=1, const_ratio=0.1):
+        super(RandomScheduling_MSE, self).__init__()
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        const_step = int(total_step*const_ratio)
+        self.prob_thres = torch.linspace(1,0, int(total_step-const_step)).to(device)
+        # dec = torch.linspace(1,0, int(total_step-const_step)).to(device)
+        # if(const_ratio!=1):
+        #     const = dec[-1] * torch.ones(const_step, device=device)
+        # else:
+        #     const = torch.zeros(int(total_step*const_ratio), device=device)
+        # self.prob_thres = torch.cat((dec, const), dim=0)
+        self.mse = nn.MSELoss()
+        self.micro_batch = micro_batch
+        self.step = 0
+        self.out = 0
+
+    def get_thres(self):
+        if self.step % self.micro_batch == 0:
+            prob = self.prob_thres[self.step//self.micro_batch] if self.step//self.micro_batch < len(self.prob_thres) else self.prob_thres[-1]
+            self.out = 1 if random.random() > prob else 0
+        self.step += 1
+        return self.out
+
+    def fcl(self, fft_pred, fft_truth):
+        # In general, FFTs here must be shifted to the center; but here we use the whole fourier space, so it is okay to no need have fourier shift operation
+        conj_pred = torch.conj(fft_pred)
+        numerator = (conj_pred*fft_truth).sum().real
+        denominator = torch.sqrt(((fft_truth).abs()**2).sum()*((fft_pred).abs()**2).sum())
+        return 1. - numerator/denominator
+
+    def fal(self, fft_pred, fft_truth):
+        return nn.MSELoss()(fft_pred.abs(), fft_truth.abs())
+
+    def cos_loss(self, fft_pred, fft_truth):
+        # Cosine Similarity Loss
+        numerator = fft_pred.real*fft_truth.real + fft_pred.imag*fft_truth.imag
+        denominator = fft_pred.abs()*fft_truth.abs() + 1E-7 
+        return (1. - numerator/denominator).mean()
+
+    def forward(self, pred, gt):
+        fft_pred = torch.fft.fftn(pred, dim=[-1,-2], norm='ortho')
+        fft_gt = torch.fft.fftn(gt, dim=[-1,-2], norm='ortho')
+        # prob = 1 if random.random() > self.prob_thres[self.step] else 0
+        prob = self.get_thres()
+
+        _, _, _, H, W = pred.shape
+        weight = np.sqrt(H*W)
+        loss = prob*self.mse(pred, gt) + (1-prob) * self.fcl(fft_pred, fft_gt)
+        loss = loss*weight
+        # self.step += 1
+        return loss
+
 # =======================================================================
 # Data Visualization
 # =======================================================================

fde28bdc201f6835ac9b552ffbfd8a7949f163c9
Vatsal Khapre <vatsalkhapre@iisc.ac.in>
2026-02-19 18:02:39 +0530

    Added gabor variant

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,57 +1,72 @@
 # CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent.py \
-#     --backbone amplinet_latent_falfcl_only_2.3.13.2.1 \
-#     --dataset cikm_latent_32 \
-#     --exp_dir cikm_latent_32_best_model \
-#     --exp_note "amplinet_latent_falfcl_only_2.3.13.2.1" \
+#     --backbone amplinet_latent_falfcl_only_2_3_13_2_gabor \
+#     --dataset meteo_lr_latent_32 \
+#     --exp_dir meteo_lr_latent_32_model_parts \
+#     --exp_note "amplinet_latent_falfcl_only_2_3_13_2_gabor" \
 #     --epochs 50 \
-#     --seq_len 15 \
-#     --frames_in 5 \
-#     --frames_out 10 \
-#     --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_CIKM.pth" \
+#     --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
 #     --valid \
 #     --num_workers 8 \
 #     --wandb_state 'online' \
 #     --wandb_project_name 'Alphapre' \
-#     --run_name 'amplinet_latent_falfcl_only_2.3.13.2.1_cikm'
+#     --run_name 'amplinet_latent_falfcl_only_2_3_13_2_gabor'
 
 # CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent.py \
-#     --backbone amplinet_latent_falfcl_only_2.3.13.2.1 \
-#     --dataset cikm_latent_32 \
-#     --exp_dir cikm_latent_32_best_model \
-#     --exp_note "amplinet_latent_falfcl_only_2.3.13.2.1" \
-#     --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_CIKM.pth" \
-#     --seq_len 15 \
-#     --frames_in 5 \
-#     --frames_out 10 \
+#     --backbone amplinet_latent_falfcl_only_2_3_13_2_gabor \
+#     --dataset meteo_lr_latent_32 \
+#     --exp_dir meteo_lr_latent_32_model_parts \
+#     --exp_note "amplinet_latent_falfcl_only_2_3_13_2_gabor" \
+#     --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
 #     --eval \
 #     --num_workers 8 \
-#     --wandb_state 'offline'
+#     --wandb_state 'offline' 
 
-CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent.py \
-    --backbone amplinet_latent_falfcl_only_2.3.13.2.2 \
+
+# CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent.py \
+#     --backbone amplinet_latent_falfcl_only_2_3_13_2_gabor \
+#     --dataset shanghai_lr_latent_32 \
+#     --exp_dir shanghai_lr_latent_32_model_parts \
+#     --exp_note "amplinet_latent_falfcl_only_2_3_13_2_gabor" \
+#     --epochs 50 \
+#     --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_SHANGHAI.pth" \
+#     --valid \
+#     --num_workers 8 \
+#     --wandb_state 'online' \
+#     --wandb_project_name 'Alphapre' \
+#     --run_name 'amplinet_latent_falfcl_only_2_3_13_2_gabor_shanghai'
+
+# CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent.py \
+#     --backbone amplinet_latent_falfcl_only_2_3_13_2_gabor \
+#     --dataset shanghai_lr_latent_32 \
+#     --exp_dir shanghai_lr_latent_32_model_parts \
+#     --exp_note "amplinet_latent_falfcl_only_2_3_13_2_gabor" \
+#     --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_SHANGHAI.pth" \
+#     --eval \
+#     --num_workers 8 \
+#     --wandb_state 'offline' 
+
+CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm_sevir_lr_latent.py \
+    --backbone amplinet_latent_falfcl_only_2_3_13_2_gabor \
     --dataset cikm_latent_32 \
-    --exp_dir cikm_latent_32_best_model \
-    --exp_note "amplinet_latent_falfcl_only_2.3.13.2.2" \
+    --exp_dir cikm_latent_32_model_parts \
+    --exp_note "amplinet_latent_falfcl_only_2_3_13_2_gabor_5_1/root6_1" \
     --epochs 50 \
+    --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
+    --valid \
     --seq_len 15 \
     --frames_in 5 \
     --frames_out 10 \
-    --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_CIKM.pth" \
-    --valid \
     --num_workers 8 \
     --wandb_state 'online' \
     --wandb_project_name 'Alphapre' \
-    --run_name 'amplinet_latent_falfcl_only_2.3.13.2.2_cikm'
+    --run_name 'amplinet_latent_falfcl_only_2.3.13.3.2_v2'
 
-CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent.py \
-    --backbone amplinet_latent_falfcl_only_2.3.13.2.2 \
-    --dataset cikm_latent_32 \
-    --exp_dir cikm_latent_32_best_model \
-    --exp_note "amplinet_latent_falfcl_only_2.3.13.2.2" \
-    --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_CIKM.pth" \
-    --seq_len 15 \
-    --frames_in 5 \
-    --frames_out 10 \
+CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm_sevir_lr_latent.py \
+    --backbone amplinet_latent_falfcl_only_2.3.13.3.2_v2 \
+    --dataset meteo_lr_latent_32 \
+    --exp_dir meteo_lr_latent_32_model_parts \
+    --exp_note "amplinet_latent_falfcl_only_2.3.13.3.2_v2" \
+    --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
     --eval \
     --num_workers 8 \
     --wandb_state 'offline'

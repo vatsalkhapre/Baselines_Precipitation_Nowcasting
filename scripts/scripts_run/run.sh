c41966c6abafdd80891cbcaa010488805f695acf
Vatsal Khapre <vatsalkhapre@iisc.ac.in>
2026-03-10 15:56:33 +0530

    Recovering mistake

---
+++ scripts/scripts_run/run.sh
@@ -0,0 +1,15 @@
+CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm.py \
+    --exp_dir cikm \
+    --exp_note lastocast_on_meteonet_pixel_space \
+    --batch_size 4 \
+    --backbone alphapre \
+    --dataset cikm \
+    --seq_len 15 \
+    --ckpt_milestone /home/vatsal/Dataserver2/ECCV26/Baselines/Alphapre_other_datasets/cikm_training_corr/checkpoints/ckpt-best.pt \
+    --eval \
+    --frames_in 5 \
+    --frames_out 10 \
+    --num_workers 8 \
+    --wandb_state 'online' \
+    --wandb_project_name 'Alphapre' \
+    --run_name "lastocast_meteonet_pixel_space" 

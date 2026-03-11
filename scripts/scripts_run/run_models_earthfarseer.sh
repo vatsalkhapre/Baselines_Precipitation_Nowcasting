f64cd847e1898b3c219cbbf8b32f3508502128a0
Vatsal Khapre <vatsalkhapre@iisc.ac.in>
2026-03-12 02:06:42 +0530

    earthfarseer

--- scripts/scripts_run/run_models_earthfarseer.sh
+++ scripts/scripts_run/run_models_earthfarseer.sh
@@ -1,15 +1,70 @@
+<<<<<<< HEAD
 CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm.py \
     --exp_dir shanghai \
     --exp_note earthfarseer_on_shanghai \
+=======
+CUDA_VISIBLE_DEVICES=2 python3 run_alphapre_convlstm.py \
+    --exp_dir sevir \
+    --exp_note earthfarseer_on_sevir \
+>>>>>>> 1ac3643 (earthfarseer)
     --batch_size 8 \
     --backbone earthfarseer \
-    --dataset shanghai \
+    --dataset sevir \
     --seq_len 25 \
     --valid \
-    --epochs 70 \
+    --epochs 40 \
     --frames_in 5 \
     --frames_out 20 \
     --num_workers 8 \
     --wandb_state 'online' \
     --wandb_project_name 'Alphapre_all_other_models' \
-    --run_name "earthfarseer_shanghai" 
+    --run_name "earthfarseer_sevir" 
+
+# CUDA_VISIBLE_DEVICES=2 python3 run_alphapre_convlstm.py \
+#     --exp_dir shanghai \
+#     --exp_note earthfarseer_on_shanghai \
+#     --batch_size 8 \
+#     --backbone earthfarseer \
+#     --dataset shanghai \
+#     --seq_len 25 \
+#     --valid \
+#     --epochs 70 \
+#     --frames_in 5 \
+#     --frames_out 20 \
+#     --num_workers 8 \
+#     --wandb_state 'online' \
+#     --wandb_project_name 'Alphapre_all_other_models' \
+#     --run_name "earthfarseer_shanghai" 
+
+
+# CUDA_VISIBLE_DEVICES=2 python3 run_alphapre_convlstm.py \
+#     --exp_dir meteonet \
+#     --exp_note earthfarseer_on_meteonet \
+#     --batch_size 8 \
+#     --backbone earthfarseer \
+#     --dataset meteo \
+#     --seq_len 25 \
+#     --valid \
+#     --epochs 50 \
+#     --frames_in 5 \
+#     --frames_out 20 \
+#     --num_workers 8 \
+#     --wandb_state 'online' \
+#     --wandb_project_name 'Alphapre_all_other_models' \
+#     --run_name "earthfarseer_meteonet" 
+
+# CUDA_VISIBLE_DEVICES=2 python3 run_alphapre_convlstm.py \
+#     --exp_dir cikm \
+#     --exp_note earthfarseer_on_cikm \
+#     --batch_size 8 \
+#     --backbone earthfarseer \
+#     --dataset cikm \
+#     --seq_len 15 \
+#     --valid \
+#     --epochs 70 \
+#     --frames_in 5 \
+#     --frames_out 10 \
+#     --num_workers 8 \
+#     --wandb_state 'online' \
+#     --wandb_project_name 'Alphapre_all_other_models' \
+#     --run_name "earthfarseer_cikm" 

88b83614bf3a52d6a97e313ae199210d2aed0699
Vatsal Khapre <vatsalkhapre@iisc.ac.in>
2026-04-08 16:47:20 +0530

    .sh earthfarseer

--- scripts/scripts_run/run_models_earthfarseer.sh
+++ scripts/scripts_run/run_models_earthfarseer.sh
@@ -30,6 +30,22 @@ CUDA_VISIBLE_DEVICES=2 python3 run_alphapre_convlstm.py \
 #     --wandb_project_name 'Alphapre_all_other_models' \
 #     --run_name "earthfarseer_sevir" 
 
+# CUDA_VISIBLE_DEVICES=2 python3 run_alphapre_convlstm.py \
+#     --exp_dir sevir \
+#     --exp_note earthfarseer_on_sevir \
+#     --batch_size 8 \
+#     --backbone earthfarseer \
+#     --dataset sevir \
+#     --seq_len 25 \
+#     --valid \
+#     --epochs 40 \
+#     --frames_in 5 \
+#     --frames_out 20 \
+#     --num_workers 8 \
+#     --wandb_state 'online' \
+#     --wandb_project_name 'Alphapre_all_other_models' \
+#     --run_name "earthfarseer_sevir" 
+
 CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm.py \
     --exp_dir shanghai \
     --exp_note earthfarseer_on_shanghai \
@@ -47,21 +63,21 @@ CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm.py \
     --run_name "earthfarseer_shanghai" 
 
 
-# CUDA_VISIBLE_DEVICES=2 python3 run_alphapre_convlstm.py \
-#     --exp_dir meteonet \
-#     --exp_note earthfarseer_on_meteonet \
-#     --batch_size 8 \
-#     --backbone earthfarseer \
-#     --dataset meteo \
-#     --seq_len 25 \
-#     --valid \
-#     --epochs 50 \
-#     --frames_in 5 \
-#     --frames_out 20 \
-#     --num_workers 8 \
-#     --wandb_state 'online' \
-#     --wandb_project_name 'Alphapre_all_other_models' \
-#     --run_name "earthfarseer_meteonet" 
+CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm.py \
+    --exp_dir meteonet \
+    --exp_note earthfarseer_on_meteonet \
+    --batch_size 4 \
+    --backbone earthfarseer \
+    --dataset meteo \
+    --seq_len 25 \
+    --valid \
+    --epochs 50 \
+    --frames_in 5 \
+    --frames_out 20 \
+    --num_workers 8 \
+    --wandb_state 'online' \
+    --wandb_project_name 'Alphapre_all_other_models' \
+    --run_name "earthfarseer_meteonet" 
 
 # CUDA_VISIBLE_DEVICES=2 python3 run_alphapre_convlstm.py \
 #     --exp_dir cikm \
--- scripts/scripts_run/run_models_lastocast_latent_space_2.sh
+++ scripts/scripts_run/run_models_lastocast_latent_space_2.sh
@@ -1,12 +1,13 @@
 CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent.py \
     --exp_dir lastocast_mse \
-    --exp_note lastocast_on_sevir_latent_space \
+    --exp_note lastocast_on_meteonet_latent_space \
     --batch_size 4 \
     --hidden_dim 64 \
     --size_factor 1.0 \
     --backbone LASTOCast_mse \
-    --dataset sevir_lr_latent_32 \
+    --dataset meteo_lr_latent_32 \
     --seq_len 25 \
+    --weight_scale 1.5 \
     --weight_scale 1.5 \
     --alpha 1.0\
     --beta 1.0 \
@@ -16,28 +17,28 @@ CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent.py \
     --frames_in 5 \
     --frames_out 20 \
     --num_workers 8 \
-    --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_SEVIR.pth" \
+    --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
     --wandb_state 'online' \
     --wandb_project_name 'Alphapre' \
     --run_name "lastocast_mse_sevir_latent_space" 
 
-# CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent.py \
-#     --exp_dir lastocast_mse \
-#     --exp_note lastocast_on_sevir_latent_space \
-#     --batch_size 4 \
-#     --hidden_dim 64 \
-#     --size_factor 1.0 \
-#     --backbone LASTOCast_mse \
-#     --dataset sevir_lr_latent_32 \
-#     --seq_len 25 \
-#     --weight_scale 1.5 \
-#     --alpha 1.0\
-#     --beta 1.0 \
-#     --freq_multiplier 2.0 \
-#     --eval \
-#     --epochs 50 \
-#     --frames_in 5 \
-#     --frames_out 20 \
-#     --num_workers 8 \
-#     --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_SEVIR.pth" \
-#     --wandb_state 'offline' 
+CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent.py \
+    --exp_dir lastocast_mse \
+    --exp_note lastocast_on_meteonet_latent_space \
+    --batch_size 4 \
+    --hidden_dim 64 \
+    --size_factor 1.0 \
+    --backbone LASTOCast_mse \
+    --dataset meteo_lr_latent_32 \
+    --seq_len 25 \
+    --weight_scale 1.5 \
+    --alpha 1.0\
+    --beta 1.0 \
+    --freq_multiplier 1.5 \
+    --eval \
+    --epochs 50 \
+    --frames_in 5 \
+    --frames_out 20 \
+    --num_workers 8 \
+    --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
+    --wandb_state 'offline' 

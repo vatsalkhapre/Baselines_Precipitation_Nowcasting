59f51d6367d6744417eb5e2924d1bddce4edeaeb
Vatsal <vatsalkhapre@iisc.ac.in>
2026-04-09 16:03:11 +0530

    Some small running changes 09-04-2026

--- scripts/scripts_novelty/run_cikm.sh
+++ scripts/scripts_novelty/run_cikm.sh
@@ -18,11 +18,11 @@ do
                                 do 
                                     for wavelet_level in 2
                                     do 
-                                    CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent_model_novelty.py \
-                                        --backbone amplinet_latent_falfcl_only_2_3_13_2_localpath_less_full_mlp_waveletsgabor2 \
+                                    CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm_sevir_lr_latent_model_novelty.py \
+                                        --backbone amplinet_latent_falfcl_only_2_3_13_2_localpath_fused_less_full_mlp_waveletsgabor2 \
                                         --dataset cikm_latent_32 \
                                         --exp_dir gabor_wavelet_model \
-                                        --exp_note "localpath_less_full_mlp_waveletsgabor2" \
+                                        --exp_note "localpath_sequential_less_full_mlp_waveletsgabor2" \
                                         --epochs 50 \
                                         --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_CIKM.pth" \
                                         --valid \
@@ -43,13 +43,13 @@ do
                                         --hf_mode 'separate' \
                                         --wandb_state 'online' \
                                         --wandb_project_name 'Alphapre' \
-                                        --run_name amplinet_latent_falfcl_only_2_3_13_2_localpath_less_full_mlp_waveletsgabor2
+                                        --run_name amplinet_latent_falfcl_only_2_3_13_2_localpath_sequential_less_full_mlp_waveletsgabor2
 
-                                    CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm_sevir_lr_latent_model_novelty.py \
-                                        --backbone amplinet_latent_falfcl_only_2_3_13_2_localpath_less_full_mlp_waveletsgabor2 \
+                                    CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm_sevir_lr_latent_model_novelty.py \
+                                        --backbone amplinet_latent_falfcl_only_2_3_13_2_localpath_fused_less_full_mlp_waveletsgabor2 \
                                         --dataset cikm_latent_32 \
                                         --exp_dir gabor_wavelet_model \
-                                        --exp_note "localpath_less_full_mlp_waveletsgabor2" \
+                                        --exp_note "localpath_sequential_less_full_mlp_waveletsgabor2" \
                                         --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_CIKM.pth" \
                                         --eval \
                                         --seq_len 15 \

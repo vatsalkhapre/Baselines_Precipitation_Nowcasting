d63ff4a1ec43c6e528c66b6f53c43163584cb8ae
Vatsal Khapre <vatsalkhapre@iisc.ac.in>
2026-04-11 13:50:24 +0530

    some coflicts resolved

--- scripts/scripts_run/run_models_phydnet.sh
+++ scripts/scripts_run/run_models_phydnet.sh
@@ -1,3 +1,39 @@
+# CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm.py \
+#     --exp_dir meteonet \
+#     --exp_note phydnet_on_meteonet \
+#     --batch_size 16 \
+#     --backbone phydnet \
+#     --dataset meteo \
+#     --seq_len 25 \
+#     --epochs 50 \
+#     --valid \
+#     --frames_in 5 \
+#     --frames_out 20 \
+#     --num_workers 8 \
+#     --wandb_state 'online' \
+#     --wandb_project_name 'Alphapre_all_other_models' \
+#     --run_name "phydnet_meteonet" 
+
+
+CUDA_VISIBLE_DEVICES=1 python3 run_alphapre_convlstm.py \
+    --exp_dir shanghai \
+    --exp_note phydnet_on_shanghai \
+    --batch_size 16 \
+    --backbone phydnet \
+    --dataset shanghai \
+    --seq_len 25 \
+    --ckpt_milestone /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exps/shanghai/phydnet_on_shanghai/checkpoints/ckpt-last.pt \
+    --res_opt \
+    --valid \
+    --epochs 70 \
+    --frames_in 5 \
+    --frames_out 20 \
+    --num_workers 8 \
+    --wandb_state 'online' \
+    --wandb_project_name 'Alphapre_all_other_models' \
+    --run_name "phydnet_shanghai" 
+
+
 # CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm.py \
 #     --exp_dir cikm \
 #     --exp_note phydnet_on_cikm \

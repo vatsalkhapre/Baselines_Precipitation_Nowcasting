d90c10b046823a93818104ed175936349c311fcd
Vatsal Khapre <vatsalkhapre@iisc.ac.in>
2026-04-18 09:38:18 +0530

    Diffcast code corrected

---
+++ scripts/scripts_run/run_models_diffcast_2.sh
@@ -0,0 +1,52 @@
+# CUDA_VISIBLE_DEVICES=0 python3 run_diffcast.py \
+#     --exp_dir meteonet \
+#     --exp_note Diffcast_on_meteonet \
+#     --batch_size 6 \
+#     --backbone phydnet \
+#     --use_diff \
+#     --dataset meteo \
+#     --seq_len 25 \
+#     --epochs 30 \
+#     --valid \
+#     --frames_in 5 \
+#     --frames_out 20 \
+#     --num_workers 8 \
+#     --wandb_state 'online' \
+#     --wandb_project_name 'Alphapre_all_other_models' \
+#     --run_name "Diffcast_meteonet" 
+
+
+# CUDA_VISIBLE_DEVICES=0 python3 run_diffcast.py \
+#     --exp_dir shanghai \
+#     --exp_note Diffcast_on_shanghai \
+#     --batch_size 6 \
+#     --backbone phydnet \
+#     --use_diff \
+#     --dataset shanghai \
+#     --seq_len 25 \
+#     --valid \
+#     --epochs 60 \
+#     --frames_in 5 \
+#     --frames_out 20 \
+#     --num_workers 8 \
+#     --wandb_state 'online' \
+#     --wandb_project_name 'Alphapre_all_other_models' \
+#     --run_name "Diffcast_shanghai" 
+
+
+CUDA_VISIBLE_DEVICES=0 python3 run_diffcast.py \
+    --exp_dir cikm \
+    --exp_note Diffcast_on_cikm \
+    --batch_size 6 \
+    --backbone phydnet \
+    --use_diff \
+    --dataset cikm \
+    --seq_len 15 \
+    --valid \
+    --epochs 60 \
+    --frames_in 5 \
+    --frames_out 10 \
+    --num_workers 8 \
+    --wandb_state 'offline' \
+    --wandb_project_name 'Alphapre_all_other_models' \
+    --run_name "Diffcast_cikm" 

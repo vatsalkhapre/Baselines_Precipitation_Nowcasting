7612684e7e4d640f0ba2120a43367fbd54e22a33
Vatsal Khapre <vatsalkhapre@iisc.ac.in>
2026-04-21 19:05:05 +0530

    conflicts resolved

--- scripts/scripts_old_model/meteo.sh
+++ scripts/scripts_old_model/meteo.sh
@@ -2,28 +2,28 @@ for cm in True
 do
     for conv_groups in 2 4 8 16
     do 
-        CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm_sevir_lr_latent.py \
-            --backbone amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2 \
-            --dataset meteo_lr_latent_32 \
-            --exp_dir meteo_lr_latent_32_old_model \
-            --exp_note "amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2_cm_${cm}_conv_gr_${conv_groups}" \
-            --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
-            --epochs 50 \
-            --valid \
-            --seq_len 25 \
-            --falfcl_weight 1 \
-            --frames_in 5 \
-            --frames_out 20 \
-            --weight_scale 1.5 \
-            --alpha 1.0 \
-            --beta 1.0 \
-            --freq_multiplier 1.5 \
-            --channel_mixing ${cm} \
-            --conv_groups ${conv_groups} \
-            --num_workers 8 \
-            --wandb_state 'online' \
-            --wandb_project_name 'Alphapre' \
-            --run_name amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2_meteonetcm_${cm}_conv_gr_${conv_groups}
+        # CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm_sevir_lr_latent.py \
+        #     --backbone amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2 \
+        #     --dataset meteo_lr_latent_32 \
+        #     --exp_dir meteo_lr_latent_32_old_model \
+        #     --exp_note "amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2_cm_${cm}_conv_gr_${conv_groups}" \
+        #     --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
+        #     --epochs 50 \
+        #     --valid \
+        #     --seq_len 25 \
+        #     --falfcl_weight 1 \
+        #     --frames_in 5 \
+        #     --frames_out 20 \
+        #     --weight_scale 1.5 \
+        #     --alpha 1.0 \
+        #     --beta 1.0 \
+        #     --freq_multiplier 1.5 \
+        #     --channel_mixing ${cm} \
+        #     --conv_groups ${conv_groups} \
+        #     --num_workers 8 \
+        #     --wandb_state 'online' \
+        #     --wandb_project_name 'Alphapre' \
+        #     --run_name amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2_meteonetcm_${cm}_conv_gr_${conv_groups}
 
         CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm_sevir_lr_latent.py \
             --backbone amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2 \
@@ -31,7 +31,7 @@ do
             --exp_dir meteo_lr_latent_32_old_model \
             --exp_note "amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2_cm_${cm}_conv_gr_${conv_groups}" \
             --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
-            --evak \
+            --eval \
             --seq_len 25 \
             --falfcl_weight 1 \
             --frames_in 5 \
@@ -52,28 +52,28 @@ for cm in False
 do
     for conv_groups in 2
     do 
-        CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm_sevir_lr_latent.py \
-            --backbone amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2 \
-            --dataset meteo_lr_latent_32 \
-            --exp_dir meteo_lr_latent_32_old_model \
-            --exp_note "amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2_cm_${cm}_conv_gr_${conv_groups}" \
-            --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
-            --epochs 50 \
-            --valid \
-            --seq_len 25 \
-            --falfcl_weight 1 \
-            --frames_in 5 \
-            --frames_out 20 \
-            --weight_scale 1.5 \
-            --alpha 1.0 \
-            --beta 1.0 \
-            --freq_multiplier 1.5 \
-            --channel_mixing ${cm} \
-            --conv_groups ${conv_groups} \
-            --num_workers 8 \
-            --wandb_state 'online' \
-            --wandb_project_name 'Alphapre' \
-            --run_name amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2_meteonetcm_${cm}_conv_gr_${conv_groups}
+        # CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm_sevir_lr_latent.py \
+        #     --backbone amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2 \
+        #     --dataset meteo_lr_latent_32 \
+        #     --exp_dir meteo_lr_latent_32_old_model \
+        #     --exp_note "amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2_cm_${cm}_conv_gr_${conv_groups}" \
+        #     --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
+        #     --epochs 50 \
+        #     --valid \
+        #     --seq_len 25 \
+        #     --falfcl_weight 1 \
+        #     --frames_in 5 \
+        #     --frames_out 20 \
+        #     --weight_scale 1.5 \
+        #     --alpha 1.0 \
+        #     --beta 1.0 \
+        #     --freq_multiplier 1.5 \
+        #     --channel_mixing ${cm} \
+        #     --conv_groups ${conv_groups} \
+        #     --num_workers 8 \
+        #     --wandb_state 'online' \
+        #     --wandb_project_name 'Alphapre' \
+        #     --run_name amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2_meteonetcm_${cm}_conv_gr_${conv_groups}
 
         CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm_sevir_lr_latent.py \
             --backbone amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2 \
@@ -81,7 +81,7 @@ do
             --exp_dir meteo_lr_latent_32_old_model \
             --exp_note "amplinet_latent_falfcl_only_2_3_13_2_convgrpscmgabor2_cm_${cm}_conv_gr_${conv_groups}" \
             --ae_ckpt_path "/home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Pretrained_ae_checkpoints/autoencoder_checkpoint_32_METEONET.pth" \
-            --evak \
+            --eval \
             --seq_len 25 \
             --falfcl_weight 1 \
             --frames_in 5 \

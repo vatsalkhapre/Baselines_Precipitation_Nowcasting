a921dde5e682e6bf557d6ac0c2e32323ebbe9f87
Vatsal Khapre <vatsalkhapre@iisc.ac.in>
2026-06-12 19:27:04 +0530

    facl loss runs

--- run_alphapre_convlstm.py
+++ run_alphapre_convlstm.py
@@ -1028,7 +1028,7 @@ class Runner(object):
             res = eval.done()
             if self.is_main and self.args.eval:
                 from utils.results_logger_csv import ResultsLogger
-                logger = ResultsLogger(csv_path="/home/vatsal/Dataserver2/ACML/newmodels.csv")
+                logger = ResultsLogger(csv_path="/home/vatsal/Dataserver2/Neurips/csv_files/models_falfcl.csv")
                 logger.log_results(
                     res_dict=res,
                     backbone=self.args.backbone,

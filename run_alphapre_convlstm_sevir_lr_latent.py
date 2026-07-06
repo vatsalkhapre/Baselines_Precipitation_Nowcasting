df46bf86cc4ef90712453794adf045de091b6417
vatsalkhapre <vatsalkhapre@iisc.ac.in>
2026-07-06 16:08:03 +0530

    latent dawncast run

--- run_alphapre_convlstm_sevir_lr_latent.py
+++ run_alphapre_convlstm_sevir_lr_latent.py
@@ -1222,7 +1222,7 @@ class Runner(object):
             res = eval.done()
             if self.is_main and self.args.eval:
                 from utils.results_logger_csv import ResultsLogger
-                logger = ResultsLogger(csv_path="/home/vatsal/Dataserver2/ACML/Final_model.csv")
+                logger = ResultsLogger(csv_path="/home/vatsal/Dataserver2/Neurips/Final_model.csv")
                 logger.log_results(
                     res_dict=res,
                     backbone=self.args.backbone,

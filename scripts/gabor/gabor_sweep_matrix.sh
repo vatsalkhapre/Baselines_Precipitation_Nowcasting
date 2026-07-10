57b2e5a0f2b7d9a039c4089c3794395f600d0705
vatsalkhapre <vatsalkhapre@iisc.ac.in>
2026-07-10 14:17:57 +0530

    shanghai gamma stat results

--- scripts/gabor/gabor_sweep_matrix.sh
+++ scripts/gabor/gabor_sweep_matrix.sh
@@ -5,6 +5,6 @@ python /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Gabor_exp/Gabor_codes
 
 python /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/Gabor_codes/gabor_sweep_matrix.py \
     --logs_glob 'Exps/gabor_exp_shanghai/*/logs/log.log' \
-    --dataset cikm --thresholds 35 40 \
+    --dataset shanghai --thresholds 35 40 \
     --out_dir /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/Gabor_explainabillity_plots/stat_gamma_shanghai
     

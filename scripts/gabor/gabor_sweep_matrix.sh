16131a296f0238244ca7c043755ecde5b355f84b
Vatsal <vatsalkhapre@iisc.ac.in>
2026-07-14 13:12:37 +0530

    cikm stat gamma

--- scripts/gabor/gabor_sweep_matrix.sh
+++ scripts/gabor/gabor_sweep_matrix.sh
@@ -1,7 +1,7 @@
 python /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/Gabor_codes/gabor_sweep_matrix.py \
     --logs_glob 'Exps/gabor_exp_cikm/*/logs/log.log' \
     --dataset cikm --thresholds 35 40 \
-    --out_dir /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Gabor_exp/Gabor_explainabillity_plots/cikm
+    --out_dir /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/Gabor_explainabillity_plots/cikm_gamma_stat
 
 python /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/Gabor_codes/gabor_sweep_matrix.py \
     --logs_glob 'Exps/gabor_exp_shanghai/*/logs/log.log' \
@@ -9,16 +9,3 @@ python /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/G
     --out_dir /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/Gabor_explainabillity_plots/shanghai_gamma_static
 
 
-
-    --dataset shanghai --thresholds 35 40 \
-    --out_dir /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/Gabor_explainabillity_plots/stat_gamma_shanghai
-
-python /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/Gabor_codes/gabor_sweep_matrix.py \
-    --logs_glob 'Exps/gabor_exp_meteo/*/logs/log.log' \
-    --dataset shanghai --thresholds 24 32 \
-    --out_dir /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/Gabor_explainabillity_plots/meteonet
-    
-python /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/Gabor_codes/gabor_sweep_matrix.py \
-    --logs_glob 'Exps/gabor_exp_meteo/*/logs/log.log' \
-    --dataset meteonet --thresholds 24 32 \
-    --out_dir /home/vatsal/NWM/Baselines_Precipitation_Nowcasting/Exp_paper/Gabor_exp/Gabor_explainabillity_plots/meteonet_const_gamma

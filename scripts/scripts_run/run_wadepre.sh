b2288471139e10327c13175ba6b40249eb52941a
Vatsal <vatsalkhapre@iisc.ac.in>
2026-08-12 16:43:21 +0530

    running new baselines and freq_sweep

--- scripts/scripts_run/run_wadepre.sh
+++ scripts/scripts_run/run_wadepre.sh
@@ -96,27 +96,27 @@ CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm.py \
 
 
 # ---------------------------- CIKM (5 -> 10, 2x rollout) ---------------------
-# CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm.py \
-#     --exp_dir cikm \
-#     --exp_note wadepre_on_cikm \
-#     --batch_size 4 \
-#     --backbone wadepre \
-#     --dataset cikm \
-#     --seq_len 15 \
-#     --epochs 200 \
-#     --valid \
-#     --img_size 128 \
-#     --img_channel 1 \
-#     --frames_in 5 \
-#     --frames_out 10 \
-#     --lr 1.5e-4 \
-#     --lr_beta1 0.9 \
-#     --lr_beta2 0.995 \
-#     --scheduler cosine \
-#     --num_workers 8 \
-#     --wandb_state 'online' \
-#     --wandb_project_name 'Alphapre_all_other_models' \
-#     --run_name "wadepre_cikm"
+CUDA_VISIBLE_DEVICES=0 python3 run_alphapre_convlstm.py \
+    --exp_dir cikm \
+    --exp_note wadepre_on_cikm \
+    --batch_size 4 \
+    --backbone wadepre \
+    --dataset cikm \
+    --seq_len 15 \
+    --epochs 150 \
+    --valid \
+    --img_size 128 \
+    --img_channel 1 \
+    --frames_in 5 \
+    --frames_out 10 \
+    --lr 1.5e-4 \
+    --lr_beta1 0.9 \
+    --lr_beta2 0.995 \
+    --scheduler cosine \
+    --num_workers 8 \
+    --wandb_state 'online' \
+    --wandb_project_name 'Alphapre_all_other_models' \
+    --run_name "wadepre_cikm"
 
 
 # ---------------------------- Evaluation (from checkpoint) -------------------
